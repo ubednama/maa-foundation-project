f3be3828e54f76086bad5f6993dbfb8657a02d8b
Anshul <ananyaj138@gmail.com>
2024-06-30 18:20:27 +0530

    Updated contact page for  resolving  design issues

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -9,8 +9,11 @@ export default function Contact() {
           <div className="flex flex-col justify-center items-center lg:items-start w-full lg:w-1/2 mb-8 lg:mb-0">
             <div className="text-neutral-950 text-3xl lg:text-5xl font-bold leading-tight lg:leading-10 text-center lg:text-left">
               Get in Touch with{" "}
-              <span className="text-red-600">Maa Foundation</span>
+              <span className="text-red-600 ml-2 lg:ml-0 mt-4 leading-snug">
+                Maa Foundation
+              </span>
             </div>
+
             <div className="flex flex-col justify-start items-start gap-4 lg:gap-11 mt-4 lg:mt-8">
               <div className="w-full lg:w-2/3 text-neutral-600 text-base lg:text-xl font-normal leading-7 text-center lg:text-left">
                 Connect with Maa Foundation for inquiries, support, or to get
@@ -32,11 +35,11 @@ export default function Contact() {
 
         {/* Contact Info Section */}
         <div className="flex flex-col lg:flex-row justify-between mt-10 lg:mt-20 mx-4 lg:mx-28">
-          <div className="flex flex-wrap justify-center items-center w-full lg:w-1/2">
-            <div className="w-full p-4">
+          <div className="flex flex-wrap justify-center  w-full lg:w-1/2 -mt-12">
+            <div className=" p-4 mt-inherit">
               <div className="w-5/6">
-                <p className="text-lg lg:text-xl font-normal leading-9 text-zinc-900 mt-2">
-                  <span className="text-lg lg:text-xl font-normal leading-9 text-red-600">
+                <p className="text-lg lg:text-xl font-normal leading-9 text-zinc-900 mt-8 ">
+                  <span className="text-lg lg:text-xl  text-red-600 mb-4">
                     MAA FOUNDATION{" "}
                   </span>
                   has been working in India for over 70 years, for the
@@ -44,15 +47,15 @@ export default function Contact() {
                   programmes in 18 states and 130+ districts.
                 </p>
               </div>
-              <div className="mt-4 space-y-4">
-                <div className="flex items-center">
-                  <div className="w-6 h-6">
+              <div className="mt-8 space-y-4">
+                <div className="flex ">
+                  <div className="w-7 h-6">
                     <img
                       src="./contact/lets-icons_message-light.jpg"
                       alt="Email Icon"
                     />
                   </div>
-                  <p className="ml-2 text-lg lg:text-xl text-zinc-900">
+                  <p className="ml-2 text-lg lg:text-xl text-zinc-900 mb-4">
                     maafoundataion@gmail.com
                   </p>
                 </div>
@@ -63,7 +66,7 @@ export default function Contact() {
                       alt="Phone Icon"
                     />
                   </div>
-                  <p className="ml-2 text-lg lg:text-xl text-zinc-900">
+                  <p className="ml-2 text-lg lg:text-xl text-zinc-900 mb-4">
                     +91 9865327856, +91 9785694325
                   </p>
                 </div>
@@ -82,8 +85,10 @@ export default function Contact() {
             </div>
           </div>
 
+          <div className="hidden lg:block w-px bg-gray-300 mx-4 lg:mx-12 my-8 lg:my-0"></div>
+
           {/* Contact Form Section */}
-          <div className="w-full lg:w-1/2 flex justify-center items-center mt-8 lg:mt-0">
+          <div className="w-full lg:w-1/2 flex justify-center  mt-8 lg:mt-0">
             <form className="w-full max-w-lg">
               <div className="space-y-3">
                 <input
