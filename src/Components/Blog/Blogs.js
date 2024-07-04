021f5674f8fb1bd488776c46068c303d46f3f701
OM TRIPATHI <omtripathi467@gmail.com>
2024-07-04 18:58:56 +0530

    blog page change

--- src/Components/Blog/Blogs.js
+++ src/Components/Blog/Blogs.js
@@ -15,39 +15,18 @@ const Blogs = () => {
         />
 
         {/* Absolute Positioned Content */}
-        <div className="pl-3 sm:pl-10 absolute inset-0 flex flex-col justify-center lg:pl-[10%] lg:py-[8%]">
-          <p className="font-semibold text-3xl lg:text-5xl text-black mb-4 lg:mb-6">
-            Blogs
-          </p>
-          <p className="font-inter text-lg lg:text-3xl font-semibold text-red-600 mb-2 lg:mb-4">
-            Voice of Empowerment
-          </p>
-          <p className="font-itim text-base lg:text-xl font-normal text-black leading-normal mb-2 lg:mb-4">
-            Amplifying Women's Strength and Influence
-          </p>
-        </div>
-
-        {/* Media Query for Smaller Dimensions */}
-        <style jsx>{`
-          @media (max-width: 499px) {
-            .lg:pl-[10%],
-            .lg:py-[8%] {
-              padding-left: 5%; /* Adjust padding for smaller screens */
-              padding-right: 5%;
-              padding-top: 8%;
-              padding-bottom: 8%;
-            }
-            .text-3xl {
-              font-size: 2rem; /* Decrease font size for smaller screens */
-            }
-            .text-lg {
-              font-size: 1.125rem;
-            }
-            .text-base {
-              font-size: 0.875rem;
-            }
-          }
-        `}</style>
+        <div className="absolute inset-0 flex flex-col justify-center p-4 sm:pl-10 lg:pl-[10%] lg:py-[8%]">
+    <p className="font-[600] text-[32px] w-[386px] sm:text-[40px] lg:text-[55px] text-black mb-2 sm:mb-4 lg:pb-10">
+      Blogs
+    </p>
+    <p className="font-[600] text-[20px] sm:text-[24px] lg:text-[27px] text-red-600 mb-1 sm:mb-2 lg:mb-4">
+      Voice of Empowerment
+    </p>
+    <p className="font-[400] font-itim text-[16px] sm:text-[20px] lg:text-[28px] text-black leading-normal mb-1 sm:mb-2 lg:mb-4">
+      Amplifying Women's Strength and Influence
+    </p>
+      </div>
+        
       </div>
 
       <div className="bg-[#ffdede59] pt-5 md:pt-10 lg:pt-20">
