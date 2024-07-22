416ff8b02d10bbb7a8614da7d9fa0a5f6fcb8dfa
Maaz <abumaaz728@gmail.com>
2024-07-22 19:40:35 +0530

    Changes Base Url

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const BASE_URL = "https://maa-foundation-backend-6.onrender.com/api";
-const BASE_URL = "http://localhost:5001/api"
+const BASE_URL = "https://maa-foundation-backend-6.onrender.com/api";
 
 const axiosInstance = axios.create({
     baseURL:BASE_URL,
